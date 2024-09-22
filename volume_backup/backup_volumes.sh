d3b2976f9b5d7148e1ae9128668aaf0ea9d05b89
argv1 <github@hofa.biz>
2024-09-22 21:55:04 +0200

    Update backup_volumes.sh

--- volume_backup/backup_volumes.sh
+++ volume_backup/backup_volumes.sh
@@ -1,22 +1,22 @@
-			#!/bin/bash
-			
-			# Verzeichnis für Backups erstellen (falls nicht vorhanden)
-			backup_root="/mnt/media/Backup"
-			backup_folder="${backup_root}/$(date +"%Y%m%d")"
-			mkdir -p "${backup_folder}"
-			
-			# Alle Docker-Volumes sichern
-			for volume in $(docker volume ls --quiet); do
-			    volume_name=$(docker volume inspect --format '{{.Name}}' "$volume")
-			    echo "Sichere Volume: $volume_name"
-			
-			    # Verzeichnis für das aktuelle Volume erstellen
-			    volume_backup_folder="${backup_folder}/${volume_name}"
-			    mkdir -p "${volume_backup_folder}"
-			
-			    # Backup erstellen
-			    docker run --rm \
-			        -v "${volume_backup_folder}:/backup" \
-			        -v "$volume:/data:ro" \
-			        debian:buster-slim bash -c "cd /data && tar -czvf /backup/${volume_name}_$(date +"%Y%m%d%H%M%S").tar.gz ."
+#!/bin/bash
+
+# Verzeichnis für Backups erstellen (falls nicht vorhanden)
+backup_root="/mnt/media/Backup"
+backup_folder="${backup_root}/$(date +"%Y%m%d")"
+mkdir -p "${backup_folder}"
+
+# Alle Docker-Volumes sichern
+for volume in $(docker volume ls --quiet); do
+    volume_name=$(docker volume inspect --format '{{.Name}}' "$volume")
+    echo "Sichere Volume: $volume_name"
+
+    # Verzeichnis für das aktuelle Volume erstellen
+    volume_backup_folder="${backup_folder}/${volume_name}"
+    mkdir -p "${volume_backup_folder}"
+
+    # Backup erstellen
+    docker run --rm \
+	-v "${volume_backup_folder}:/backup" \
+	-v "$volume:/data:ro" \
+	debian:buster-slim bash -c "cd /data && tar -czvf /backup/${volume_name}_$(date +"%Y%m%d%H%M%S").tar.gz ."
 done
